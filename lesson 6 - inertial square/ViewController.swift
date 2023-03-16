2a5478369df854b044ccd026e18fbc4f1a7ff3bb
LBkos <constantin.l.o.p@gmail.com>
2023-03-16 14:58:49 +0700

    Initial Commit

---
+++ lesson 6 - inertial square/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 6 - inertial square
+//
+//  Created by Константин Лопаткин on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
